4b8fb869de56ea65f42ea4210b3f264c5d8a5564
Grzegorz Wilczura <grzegorz.wilczura@gmail.com>
2025-01-03 21:18:19 +0100

    add elastic apm

--- src/Wilczura.Common/Wilczura.Common.Web/Middleware/LoggingMiddleware.cs
+++ src/Wilczura.Common/Wilczura.Common.Web/Middleware/LoggingMiddleware.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Elastic.Apm;
+using Elastic.Apm.Api;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
 using Wilczura.Common.Consts;
 using Wilczura.Common.Logging;
+using Wilczura.Common.Models;
 using Wilczura.Common.Web.Extensions;
 
 namespace Wilczura.Common.Web.Middleware;
@@ -10,11 +14,13 @@ public class LoggingMiddleware
 {
     private readonly RequestDelegate _next;
     private readonly ILogger<LoggingMiddleware> _logger;
+    private readonly CustomOptions _customOptions;
 
-    public LoggingMiddleware(RequestDelegate next, ILogger<LoggingMiddleware> logger)
+    public LoggingMiddleware(RequestDelegate next, ILogger<LoggingMiddleware> logger, CustomOptions customOptions)
     {
         _next = next;
         _logger = logger;
+        _customOptions = customOptions;
     }
 
     public async Task InvokeAsync(HttpContext context)
@@ -23,42 +29,64 @@ public class LoggingMiddleware
         var message = "Http in";
         var activityName = "http-in";
         var logInfo = new LogInfo(message, ObservabilityConsts.EventCategoryWeb);
-        logInfo.EventAction = activityName;
-        var logScope = new LogScope(_logger, logInfo, LogLevel.Information, LogEvents.WebRequest, activityName: activityName);
-        logInfo.HttpMethod = context.Request.Method;
-        try
+
+        Func<Task> action = async () =>
         {
-            await _next(context);
+            logInfo.EventAction = activityName;
+            var logScope = new LogScope(_logger, logInfo, LogLevel.Information, LogEvents.WebRequest, activityName: activityName);
+            logInfo.HttpMethod = context.Request.Method;
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+                throw;
+            }
+            finally
+            {
+                logInfo.EventReason = $"HTTP Code {context.Response.StatusCode}";
+
+                if (exception != null)
+                {
+                    logInfo.ApplyException(exception);
+                }
+                else if (context.Response.StatusCode >= 400)
+                {
+                    logInfo.EventOutcome = ObservabilityConsts.EventOutcomeFailure;
+                }
+                else if (context.Response.StatusCode >= 300)
+                {
+                    logInfo.EventOutcome = ObservabilityConsts.EventOutcomeUnknown;
+                }
+                else
+                {
+                    logInfo.EventOutcome = ObservabilityConsts.EventOutcomeSuccess;
+                }
+
+                logInfo.ApplyPrincipal(context.GetPrincipal());
+
+                logScope.Dispose();
+            }
+        };
+
+        if (_customOptions.EnableApm)
+        {
+            var containsTraceParentHeader =
+                context.Request.Headers.TryGetValue(HeaderNames.TraceParent, out var traceParentHeader);
+            DistributedTracingData? currentTraceData = null;
+
+            if (containsTraceParentHeader)
+            {
+                currentTraceData = DistributedTracingData.TryDeserializeFromString(traceParentHeader);
+            }
+
+            await Agent.Tracer.CaptureTransaction(activityName, nameof(LogEvents.WebRequest), action, currentTraceData);
         }
-        catch (Exception ex)
+        else
         {
-            exception = ex;
-            throw;
-        }
-        finally
-        {
-            logInfo.EventReason = $"HTTP Code {context.Response.StatusCode}";
-
-            if (exception != null)
-            {
-                logInfo.ApplyException(exception);
-            }
-            else if (context.Response.StatusCode >= 400)
-            {
-                logInfo.EventOutcome = ObservabilityConsts.EventOutcomeFailure;
-            }
-            else if (context.Response.StatusCode >= 300)
-            {
-                logInfo.EventOutcome = ObservabilityConsts.EventOutcomeUnknown;
-            }
-            else
-            {
-                logInfo.EventOutcome = ObservabilityConsts.EventOutcomeSuccess;
-            }
-
-            logInfo.ApplyPrincipal(context.GetPrincipal());
-
-            logScope.Dispose();
+            await action();
         }
     }
 }
